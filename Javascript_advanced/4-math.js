6a908ed55759bc5ca6f5fed8c869130cea96b155
TuyenH <thtuyen199@gmail.com>
2025-04-11 16:39:37 +0200

    remove console.log of the function

--- Javascript_advanced/4-math.js
+++ Javascript_advanced/4-math.js
@@ -11,13 +11,10 @@ function addBy(firstNumber) {
 }
 
 let addBy100 = addBy(100);
-console.log(addBy(100));
 
 let addBy1000 = addBy(1000);
-console.log(divideBy(1000));
 
 let divideBy10 = divideBy(10);
-console.log(divideBy(10));
 
 let divideBy100 = divideBy(100);
-console.log(divideBy(100));
+
